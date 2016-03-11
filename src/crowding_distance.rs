c8107865c39c9f93fa5deb4e35c5aa2b0449743b
Michael Neumann <mneumann@ntecs.de>
2016-03-11 11:44:26 +0100

    Simplify the code by removing one extra indirection. work directly on SolutionRankDist.

--- src/crowding_distance.rs
+++ src/crowding_distance.rs
@@ -39,42 +39,41 @@ pub fn crowding_distance_assignment<P: MultiObjective>(solutions: &[P],
                                                        -> Vec<SolutionRankDist> {
     assert!(num_objectives > 0);
 
-    let l = individuals_idx.len();
-    let mut distance: Vec<f32> = (0..l).map(|_| 0.0).collect();
-    let mut indices: Vec<usize> = (0..l).map(|i| i).collect();
+    let mut s: Vec<_> = individuals_idx.iter()
+                                       .map(|&i| {
+                                           SolutionRankDist {
+                                               rank: common_rank,
+                                               dist: 0.0,
+                                               idx: i,
+                                           }
+                                       })
+                                       .collect();
+    let l = s.len();
 
     for m in 0..num_objectives {
         // sort using objective `m`
-        indices.sort_by(|&a, &b| {
-            solutions[individuals_idx[a]].cmp_objective(&solutions[individuals_idx[b]], m)
-        });
-        distance[indices[0]] = f32::INFINITY;
-        distance[indices[l - 1]] = f32::INFINITY;
+        s.sort_by(|a, b| solutions[a.idx].cmp_objective(&solutions[b.idx], m));
 
-        let min_idx = individuals_idx[indices[0]];
-        let max_idx = individuals_idx[indices[l - 1]];
+        // assign infinite crowding distance to edges.
+        s[0].dist = f32::INFINITY;
+        s[l - 1].dist = f32::INFINITY;
+
+        let min_idx = s[0].idx;
+        let max_idx = s[l - 1].idx;
 
         let dist_max_min = solutions[max_idx].dist_objective(&solutions[min_idx], m);
         if dist_max_min != 0.0 {
             let norm = num_objectives as f32 * dist_max_min;
             debug_assert!(norm != 0.0);
             for i in 1..(l - 1) {
-                let next_idx = individuals_idx[indices[i + 1]];
-                let prev_idx = individuals_idx[indices[i - 1]];
+                let next_idx = s[i + 1].idx;
+                let prev_idx = s[i - 1].idx;
                 let dist = solutions[next_idx].dist_objective(&solutions[prev_idx], m);
                 debug_assert!(dist >= 0.0);
-                distance[indices[i]] += dist / norm;
+                s[i].dist += dist / norm;
             }
         }
     }
 
-    return indices.iter()
-                  .map(|&i| {
-                      SolutionRankDist {
-                          idx: individuals_idx[i],
-                          rank: common_rank,
-                          dist: distance[i],
-                      }
-                  })
-                  .collect();
+    return s;
 }
