fc58c66eab23e3fcbf6cf2cd480ca87c5544e987
Michael Neumann <mneumann@ntecs.de>
2018-02-04 17:48:58 +0100

    Simplify tests

--- src/objective.rs
+++ src/objective.rs
@@ -37,69 +37,17 @@ pub trait Objective {
     fn distance(&self, a: &Self::Solution, b: &Self::Solution) -> Self::Distance;
 }
 
-#[cfg(test)]
-mod tests {
-    use std::cmp::Ordering;
-    use super::Objective;
+#[test]
+fn test_objectives() {
+    use test_helper_objective::{Objective1, Objective2, Objective3, Tuple};
 
-    // Our multi-variate fitness/solution value
-    struct Tuple(usize, usize);
+    let a = &Tuple(1, 2);
+    let b = &Tuple(2, 1);
+    assert_eq!(Ordering::Less, Objective1.total_order(a, b));
+    assert_eq!(Ordering::Greater, Objective2.total_order(a, b));
+    assert_eq!(Ordering::Equal, Objective3.total_order(a, b));
 
-    // We define three objectives
-    struct Objective1;
-    struct Objective2;
-    struct Objective3;
-
-    impl Objective for Objective1 {
-        type Solution = Tuple;
-        type Distance = f64;
-
-        fn total_order(&self, a: &Self::Solution, b: &Self::Solution) -> Ordering {
-            a.0.cmp(&b.0)
-        }
-
-        fn distance(&self, a: &Self::Solution, b: &Self::Solution) -> Self::Distance {
-            (a.0 as f64) - (b.0 as f64)
-        }
-    }
-
-    impl Objective for Objective2 {
-        type Solution = Tuple;
-        type Distance = f64;
-
-        fn total_order(&self, a: &Self::Solution, b: &Self::Solution) -> Ordering {
-            a.1.cmp(&b.1)
-        }
-
-        fn distance(&self, a: &Self::Solution, b: &Self::Solution) -> Self::Distance {
-            (a.1 as f64) - (b.1 as f64)
-        }
-    }
-
-    // Objective3 is defined on the sum of the tuple values.
-    impl Objective for Objective3 {
-        type Solution = Tuple;
-        type Distance = f64;
-
-        fn total_order(&self, a: &Self::Solution, b: &Self::Solution) -> Ordering {
-            (a.0 + a.1).cmp(&(b.0 + b.1))
-        }
-
-        fn distance(&self, a: &Self::Solution, b: &Self::Solution) -> Self::Distance {
-            (a.0 + a.1) as f64 - (b.0 + b.1) as f64
-        }
-    }
-
-    #[test]
-    fn test_objectives() {
-        let a = &Tuple(1, 2);
-        let b = &Tuple(2, 1);
-        assert_eq!(Ordering::Less, Objective1.total_order(a, b));
-        assert_eq!(Ordering::Greater, Objective2.total_order(a, b));
-        assert_eq!(Ordering::Equal, Objective3.total_order(a, b));
-
-        assert_eq!(-1.0, Objective1.distance(a, b));
-        assert_eq!(1.0, Objective2.distance(a, b));
-        assert_eq!(0.0, Objective3.distance(a, b));
-    }
+    assert_eq!(-1.0, Objective1.distance(a, b));
+    assert_eq!(1.0, Objective2.distance(a, b));
+    assert_eq!(0.0, Objective3.distance(a, b));
 }
