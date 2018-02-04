92fcc6c1115fd02c1b50bf3e5b3cd4c05f7b1830
Michael Neumann <mneumann@ntecs.de>
2018-02-04 10:30:53 +0100

    Specify DominationOrd trait

--- src/domination.rs
+++ src/domination.rs
@@ -1,87 +1,99 @@
 use std::cmp::Ordering;
 
-/// The dominance relation between two items.
+/// The dominance relation between two solutions. There are three possible situations:
+///
+/// - Either solution `a` dominates solution `b` ("a < b")
+/// - Or solution `b` dominates solution `a` ("a > b")
+/// - Or neither solution `a` nor `b` dominates each other ("a == b")
+///
+pub trait DominationOrd {
+    /// The solution value type on which the dominance relation is defined.
+    type Solution;
 
-pub trait Domination {
-    /// Returns true if `self` dominates `other`, taking only `objectives` into account.
-
-    fn dominates(&self, other: &Self, objectives: &[usize]) -> bool {
-        match self.domination_ord(other, objectives) {
-            Ordering::Less => true,
-            _ => false,
-        }
-    }
-
-    /// Returns the domination order, taking only `objectives` into account.
-    fn domination_ord(&self, other: &Self, objectives: &[usize]) -> Ordering {
-        if self.dominates(other, objectives) {
+    /// Returns the domination order.
+    fn domination_ord(&self, a: &Self::Solution, b: &Self::Solution) -> Ordering {
+        if self.dominates(a, b) {
             Ordering::Less
-        } else if other.dominates(self, objectives) {
+        } else if self.dominates(b, a) {
             Ordering::Greater
         } else {
             Ordering::Equal
+        }
+    }
+
+    /// Returns true if `a` dominates `b` ("a < b").
+    fn dominates(&self, a: &Self::Solution, b: &Self::Solution) -> bool {
+        match self.domination_ord(a, b) {
+            Ordering::Less => true,
+            _ => false,
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use sort::{fast_non_dominated_sort, FastNonDominatedSorter};
-    use super::Domination;
+    use super::DominationOrd;
     use std::cmp::Ordering;
 
-    struct T(u32, u32);
+    // Our multi-variate fitness/solution value
+    struct Tuple(usize, usize);
 
-    impl Domination for T {
-        fn domination_ord(&self, other: &Self, _objectives: &[usize]) -> Ordering {
-            if self.0 < other.0 && self.1 <= other.1 {
-                return Ordering::Less;
-            }
-            if self.0 <= other.0 && self.1 < other.1 {
-                return Ordering::Less;
-            }
+    // We can have multiple dominance relations defined on a single
+    // type, without having to wrap the "Tuple" itself.
+    struct TupleDominationOrd;
 
-            if self.0 > other.0 && self.1 >= other.1 {
-                return Ordering::Greater;
-            }
-            if self.0 >= other.0 && self.1 > other.1 {
-                return Ordering::Greater;
-            }
+    impl DominationOrd for TupleDominationOrd {
+        type Solution = Tuple;
 
-            return Ordering::Equal;
+        fn domination_ord(&self, a: &Self::Solution, b: &Self::Solution) -> Ordering {
+            if a.0 < b.0 && a.1 <= b.1 {
+                Ordering::Less
+            } else if a.0 <= b.0 && a.1 < b.1 {
+                Ordering::Less
+            } else if a.0 > b.0 && a.1 >= b.1 {
+                Ordering::Greater
+            } else if a.0 >= b.0 && a.1 > b.1 {
+                Ordering::Greater
+            } else {
+                Ordering::Equal
+            }
         }
     }
 
     #[test]
-    fn test_dominate() {
-        assert_eq!(Ordering::Equal, T(1, 2).domination_ord(&T(1, 2), &[0, 1]));
-        assert_eq!(Ordering::Equal, T(1, 2).domination_ord(&T(2, 1), &[0, 1]));
-        assert_eq!(Ordering::Less, T(1, 2).domination_ord(&T(1, 3), &[0, 1]));
-        assert_eq!(Ordering::Less, T(0, 2).domination_ord(&T(1, 2), &[0, 1]));
-        assert_eq!(Ordering::Greater, T(1, 3).domination_ord(&T(1, 2), &[0, 1]));
-        assert_eq!(Ordering::Greater, T(1, 2).domination_ord(&T(0, 2), &[0, 1]));
+    fn test_non_domination() {
+        let a = &Tuple(1, 2);
+        let b = &Tuple(2, 1);
+
+        // Non-domination due to reflexitivity
+        assert_eq!(Ordering::Equal, TupleDominationOrd.domination_ord(a, a));
+        assert_eq!(Ordering::Equal, TupleDominationOrd.domination_ord(b, b));
+
+        // Non-domination
+        assert_eq!(Ordering::Equal, TupleDominationOrd.domination_ord(a, b));
+        assert_eq!(Ordering::Equal, TupleDominationOrd.domination_ord(b, a));
     }
 
     #[test]
-    fn test_non_dominated_sort() {
-        let solutions = vec![T(1, 2), T(1, 2), T(2, 1), T(1, 3), T(0, 2)];
-        let fronts = fast_non_dominated_sort(&solutions, solutions.len(), &[0, 1]);
+    fn test_domination() {
+        let a = &Tuple(1, 2);
+        let b = &Tuple(1, 3);
+        let c = &Tuple(0, 2);
 
-        assert_eq!(3, fronts.len());
-        assert_eq!(&vec![2, 4], &fronts[0]);
-        assert_eq!(&vec![0, 1], &fronts[1]);
-        assert_eq!(&vec![3], &fronts[2]);
+        // a < b
+        assert_eq!(Ordering::Less, TupleDominationOrd.domination_ord(a, b));
+        // c < a
+        assert_eq!(Ordering::Less, TupleDominationOrd.domination_ord(c, a));
+        // transitivity => c < b
+        assert_eq!(Ordering::Less, TupleDominationOrd.domination_ord(c, b));
+
+        // Just reverse the relation: forall a, b: a < b => b > a
+
+        // b > a
+        assert_eq!(Ordering::Greater, TupleDominationOrd.domination_ord(b, a));
+        // a > c
+        assert_eq!(Ordering::Greater, TupleDominationOrd.domination_ord(a, c));
+        // transitivity => b > c
+        assert_eq!(Ordering::Greater, TupleDominationOrd.domination_ord(b, c));
     }
-
-    #[test]
-    fn test_non_dominated_sort_iter() {
-        let solutions = vec![T(1, 2), T(1, 2), T(2, 1), T(1, 3), T(0, 2)];
-        let mut fronts = FastNonDominatedSorter::new(&solutions, &|s| s, &[0, 1]);
-
-        assert_eq!(Some(vec![2, 4]), fronts.next());
-        assert_eq!(Some(vec![0, 1]), fronts.next());
-        assert_eq!(Some(vec![3]), fronts.next());
-        assert_eq!(None, fronts.next());
-    }
-
 }
