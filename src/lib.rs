9752539567866408edf97008f6bf1396df0e9083
Michael Neumann <mneumann@ntecs.de>
2018-02-04 14:53:23 +0100

    MultiObjective now uses trait objects
    Trait objects use vtables, i.e. runtime method dispatching (indirect
    call). Before, we were passing the objective as a number into every
    method of Objective and doing a long chain of if's there (basically a
    switch on every possible objective number).  Using trait objects is
    easier and should be much faster.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
 extern crate non_dominated_sort;
 
 pub mod objective;
+pub mod multi_objective;
+
+#[cfg(test)]
+mod test_helper_objective;
--- src/multi_objective.rs
+++ src/multi_objective.rs
@@ -1,123 +1,34 @@
-use std::cmp::Ordering;
-use std::ops::Sub;
-use std::convert::From;
-use domination::Domination;
+use objective::Objective;
+use std::marker::PhantomData;
 
-pub trait MultiObjective: Send {
-    fn cmp_objective(&self, other: &Self, objective: usize) -> Ordering;
+pub struct MultiObjective<'a, S, D>
+where
+    S: 'a,
+    D: 'a,
+{
+    objectives: &'a [&'a Objective<Solution = S, Distance = D>],
+    _solution: PhantomData<S>,
+    _distance: PhantomData<D>,
+}
 
-    /// Calculates the distance of objective between self and other
-    fn dist_objective(&self, other: &Self, objective: usize) -> f64;
-
-    fn similar_to(
-        &self,
-        other: &Self,
-        objectives: &[usize],
-        min_max_objective_distances: &[f64],
-        objective_eps: f64,
-    ) -> bool {
-        assert!(objectives.len() == min_max_objective_distances.len());
-        let mut eq_cnt = 0;
-        for (&i, &min_max_dist) in objectives.iter().zip(min_max_objective_distances.iter()) {
-            let dist = self.dist_objective(other, i).abs();
-            if min_max_dist == 0.0 {
-                if dist == 0.0 {
-                    // XXX: This could be removed, as it should be equal
-                    eq_cnt += 1;
-                }
-            } else {
-                assert!(min_max_dist > 0.0);
-                debug_assert!(dist >= 0.0);
-                if (dist / min_max_dist) < objective_eps {
-                    eq_cnt += 1;
-                }
-            }
+impl<'a, S, D> MultiObjective<'a, S, D>
+where
+    S: 'a,
+    D: 'a,
+{
+    pub fn new(objectives: &'a [&'a Objective<Solution = S, Distance = D>]) -> Self {
+        Self {
+            objectives,
+            _solution: PhantomData,
+            _distance: PhantomData,
         }
-        eq_cnt == objectives.len()
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct MultiObjective2<T>
-where
-    T: Sized + PartialOrd + Copy + Clone + Send,
-{
-    pub objectives: [T; 2],
-}
-
-impl<T> From<(T, T)> for MultiObjective2<T>
-where
-    T: Sized + PartialOrd + Copy + Clone + Send,
-{
-    #[inline]
-    fn from(t: (T, T)) -> MultiObjective2<T> {
-        MultiObjective2 {
-            objectives: [t.0, t.1],
-        }
-    }
-}
-
-impl<T, R> MultiObjective for MultiObjective2<T>
-where
-    T: Copy + PartialOrd + Sub<Output = R> + Send,
-    R: Into<f64>,
-{
-    #[inline]
-    fn cmp_objective(&self, other: &Self, objective: usize) -> Ordering {
-        self.objectives[objective]
-            .partial_cmp(&other.objectives[objective])
-            .unwrap()
-    }
-    #[inline]
-    fn dist_objective(&self, other: &Self, objective: usize) -> f64 {
-        (self.objectives[objective] - other.objectives[objective]).into()
-    }
-}
-
-pub fn dominates_helper<T>(lhs: &T, rhs: &T, objectives: &[usize]) -> bool
-where
-    T: MultiObjective,
-{
-    let mut less_cnt = 0;
-    for &i in objectives.iter() {
-        match lhs.cmp_objective(rhs, i) {
-            Ordering::Greater => {
-                return false;
-            }
-            Ordering::Less => {
-                less_cnt += 1;
-            }
-            Ordering::Equal => {}
-        }
-    }
-    return less_cnt > 0;
-}
-
-impl<T, R> Domination for MultiObjective2<T>
-where
-    T: Copy + PartialOrd + Sub<Output = R> + Send,
-    R: Into<f64>,
-{
-    fn dominates(&self, other: &Self, objectives: &[usize]) -> bool {
-        dominates_helper(self, other, objectives)
     }
 }
 
 #[test]
-fn test_dominates() {
-    let a = MultiObjective2::from((1.0f32, 0.1));
-    let b = MultiObjective2::from((0.1f32, 0.1));
-    let c = MultiObjective2::from((0.1f32, 1.0));
+fn test_multi_objective() {
+    use test_helper_objective::{Objective1, Objective2, Objective3, Tuple};
 
-    assert_eq!(false, a.dominates(&a, &[0, 1]));
-    assert_eq!(false, a.dominates(&b, &[0, 1]));
-    assert_eq!(false, a.dominates(&c, &[0, 1]));
-
-    assert_eq!(true, b.dominates(&a, &[0, 1]));
-    assert_eq!(false, b.dominates(&b, &[0, 1]));
-    assert_eq!(true, b.dominates(&c, &[0, 1]));
-
-    assert_eq!(false, c.dominates(&a, &[0, 1]));
-    assert_eq!(false, c.dominates(&b, &[0, 1]));
-    assert_eq!(false, c.dominates(&c, &[0, 1]));
+    // construct a multi objective over a Tuple
+    let _ = MultiObjective::<Tuple, f64>::new(&[&Objective1, &Objective2, &Objective3]);
 }
